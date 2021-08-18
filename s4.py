8950e54b41c3b9357ced08fced5a9799017cebb0
farzanehsalari <87785351+farzanehsalari@users.noreply.github.com>
2021-08-18 22:24:12 +0430

    Create s4.py

---
+++ s4.py
@@ -0,0 +1,11 @@
+add1 = int(input ())
+add2 = int(input())
+amal = input("vared konid amalgar\n")
+if amal == "+":
+  print (add1 + add2)
+elif amal == "-":
+  print (add1 - add2)
+elif amal == "*":
+  print (add1 * add2)
+else :
+  print ("nashenas hast")
